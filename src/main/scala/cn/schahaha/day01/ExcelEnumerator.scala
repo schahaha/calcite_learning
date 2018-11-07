95ea363c0817bc8f448b8b20142e023f891e82ae
schahaha <shicheng31604@gmail.com>
2018-11-07 15:45:17 +0800

    迭代器的方法还需要继续完善..

--- src/main/scala/cn/schahaha/day01/ExcelEnumerator.scala
+++ src/main/scala/cn/schahaha/day01/ExcelEnumerator.scala
@@ -9,7 +9,16 @@ import org.apache.poi.xssf.usermodel.{XSSFCell, XSSFRow, XSSFSheet, XSSFWorkbook
 
 import scala.collection.mutable.ListBuffer
 
+/**
+  * 迭代器
+  *
+  * @param file
+  * @param types
+  * @tparam E
+  */
 class ExcelEnumerator[E](val file:File,val types:java.util.List[RelDataType]) extends Enumerator[E]{
+
+  //todo  完成迭代器的方法
   override def moveNext(): Boolean = ???
 
   override def current(): E = ???
